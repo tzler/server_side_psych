5c3565856331c41239935f37bff385fe77ee5024
tzler <tyler.ray.bonnen@gmail.com>
2019-01-06 06:20:32 +0000

    hello_world: communication

--- experiment/hello_world/app.js
+++ experiment/hello_world/app.js
@@ -1,12 +1,17 @@
-// SETUP MODULES 
+// IMPORT MODULES 
 // npm install express
 const express = require('express');
 // built in 'file system' operations
 const fs = require('fs')
 // express instance--'app' is the convention 
 const app = express();
+// mongo database modules
+const mongo_client = require('mongodb').MongoClient;
+// assertion helpers
+const assert = require('assert');
 
-// SETUP MONGO AND SSL TRAFFIC PERMISSIONS
+
+// SETUP TRAFFIC PERMISSIONS (MONGO/HTTPS/firewall)
 // firewall permitted port we'll use
 const external_port = 8889;
 // location of ssl and mongo credentials
@@ -16,16 +21,16 @@ const options = {
 	key:  fs.readFileSync(credentials + "ssl_privatekey"), 
 	cert: fs.readFileSync(credentials + "ssl_certificate")
 };
-
-// SET UP MONGO CONNECTION
+// setup server-side port using credentials 
+const server = require('https').createServer(options,app)
+// extract mongo authentification keys
 const db_key = JSON.parse(fs.readFileSync(credentials + 'mongo_keys')); 
+// construct string from port and authentification data
 const mongo_url = `mongodb://${db_key.user}:${db_key.pwd}@localhost:27017/`;
-console.log(mongo_url)
-var  mongo_client = require('mongodb').MongoClient;
-const assert = require('assert');
 
-// CONNECT TO MONGO DATABASE
-// just establish a connection -- nothing else yet
+
+// ESTABLISH MONGO CONNECTION
+// nothing helpful yet, just connect
 mongo_client.connect(mongo_url, function(err, client) {
     // make sure we didn't throw an error
     assert.equal(null, err);
@@ -35,15 +40,11 @@ mongo_client.connect(mongo_url, function(err, client) {
 
 
 // SETUP SERVER-SIDE OPERATIONS 
-// setup server-side port using credentials 
-const server = require('https').createServer(options,app)
-		
 // open port on the serverd 
 server.listen(external_port, function() {
   console.log('running on port ', external_port) 
   }
 )
-
 // listen to incoming requests
 app.get('/', function (req, res) {
   // client response protocol
