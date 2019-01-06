373ef047503658203935decfae57674f7d8913fc
tzler <tyler.ray.bonnen@gmail.com>
2019-01-06 05:59:47 +0000

    hello_world: incorporate mongo and authentication keys

--- experiment/hello_world/app.js
+++ experiment/hello_world/app.js
@@ -1,5 +1,4 @@
 // SETUP MODULES 
-//
 // npm install express
 const express = require('express');
 // built in 'file system' operations
@@ -8,9 +7,8 @@ const fs = require('fs')
 const app = express();
 
 // SETUP MONGO AND SSL TRAFFIC PERMISSIONS
-//
 // firewall permitted port we'll use
-const port_location = 8889;
+const external_port = 8889;
 // location of ssl and mongo credentials
 const credentials = 'credentials/'
 // extract relevant info from SSL key and certification
@@ -19,14 +17,30 @@ const options = {
 	cert: fs.readFileSync(credentials + "ssl_certificate")
 };
 
+// SET UP MONGO CONNECTION
+const db_key = JSON.parse(fs.readFileSync(credentials + 'mongo_keys')); 
+const mongo_url = `mongodb://${db_key.user}:${db_key.pwd}@localhost:27017/`;
+console.log(mongo_url)
+var  mongo_client = require('mongodb').MongoClient;
+const assert = require('assert');
+
+// CONNECT TO MONGO DATABASE
+// just establish a connection -- nothing else yet
+mongo_client.connect(mongo_url, function(err, client) {
+    // make sure we didn't throw an error
+    assert.equal(null, err);
+    console.log('connected to database :D')
+  }
+);
+
+
 // SETUP SERVER-SIDE OPERATIONS 
-// 
 // setup server-side port using credentials 
 const server = require('https').createServer(options,app)
 		
 // open port on the serverd 
-server.listen(port_location, function() {
-  console.log('running on port ', port_location) 
+server.listen(external_port, function() {
+  console.log('running on port ', external_port) 
   }
 )
 
