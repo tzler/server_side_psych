bafefab7878cd9cd3f395ed51d6583c0cefe3f9e
tzler <tyler.ray.bonnen@gmail.com>
2019-01-12 22:23:39 +0000

    instructions

--- server_setup/test.js
+++ server_setup/test.js
@@ -2,11 +2,12 @@ const http = require("http");
 const test_port = 8888
 
 // create a server on 'test_port'
-http.createServer(function (request, response) {
+const open_port = http.createServer(function (request, response) {
   // Send the response body as "Hello World"
   response.end('Great! NodeJS is running on your server :)\n');
   console.log(':::server received client\'s request:::')
-}).listen(test_port);
-
+})
+  
+open_port.listen(test_port);
 // Console will print the message
 console.log('\n\tServer running at http://<YOUR.IP.ADDRESS>:8888/\n');
